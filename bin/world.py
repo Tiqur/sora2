00917e15216939eaf6d4278fceb300e40b17b71d
Tiqur <Tiqur7@gmail.com>
2021-09-01 23:42:25 -0700

    Fix integer not overflowing (emulating casting to int)

--- bin/world.py
+++ bin/world.py
@@ -1,10 +1,19 @@
+from bin.random import Random;
+import numpy as np
 
 # Emulate world generation
 class World:
-    def __init(self):
-        self._seed;
-        self._random;
+    def __init__(self, seed):
+        self._seed = seed;
+        self._random = Random();
 
+    def _is_slime_chunk(self, xPosition, zPosition):
+        self._random.set_seed(
+          np.int32(self._seed) +
+          np.int32(xPosition * xPosition * 0x4c1906) +
+          np.int32(xPosition * 0x5ac0db) +
+          np.int32(zPosition * zPosition) * 0x4307a7 +
+          np.int32(zPosition * 0x5f24f) ^ 0x3ad8025f
+        );
 
-
-
+        return self._random.next_int(10) == 0;
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bin.world import World;
 
-w = World(1)
+w = World(1);
+print(w._is_slime_chunk(181, -85));
 
 

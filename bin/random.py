a20309e1366d8b88f14c54a16a647b77a454bbff
Tiqur <Tiqur7@gmail.com>
2021-09-01 22:24:12 -0700

    added next_int method

--- bin/random.py
+++ bin/random.py
@@ -1,34 +1,44 @@
 
 # Emulate Java's random class for world generation
 class Random:
-    def __init__(self, seed=None):
+    def __init__(self):
         self._multiplier = 0x5DEECE66D;
         self._addend = 0xB;
         self._mask = (1 << 48) - 1;
-        self._seed = seed;
-
-    def test(self):
-        print(self._seed);
-        print(self._multiplier);
-        print(self._addend);
-        print(self._mask);
-        print(self.initial_scramble(self._seed));
-
-    # Set the seed of the random number generator
-    def set_seed(self, seed):
-        self._seed = seed;
+        self._seed = 0;
 
     # Scramble initial seed
-    def initial_scramble(self, seed):
+    def _initial_scramble(self, seed):
         return (seed ^ self._multiplier) & self._mask;
 
     # Generate next psuedo random number
-    def next(bits):
-        this._seed = (this._seed * this._multiplier + this._addend) & this._mask;
-        return (this._seed >> (48 - bits));
+    def _next(self, bits):
+        self._seed = (self._seed * self._multiplier + self._addend) & self._mask;
+        return (self._seed >> (48 - bits));
+
+    # Set the seed of the random number generator
+    def set_seed(self, seed):
+        self._seed = self._initial_scramble(seed);
+
+    # Generate next pseudorandom int ( public )
+    def next_int(self, n=32):
+        # Make sure n is positive
+        if (n <= 0):
+            raise Exception('n must be positive');
+
+        # If n is a power of 2
+        if (n & -n) == n:
+            return ((n * self._next(31)) >> 31);
+
+        condition = True;
+        while condition:
+            bits = self._next(31);
+            val = bits % n;
+            condition = bits - val + (n-1) < 0;
+
+        return val;
 
 
 
 
-    
-
+            
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bin.random import Random;
 
-r = Random(1);
-r.test();
+r = Random();
+r.set_seed(200);
+print(r.next_int(10))

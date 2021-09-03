2c787296981cccc32fae737a36b18776ce14b8da
Tiqur <Tiqur7@gmail.com>
2021-09-03 00:33:42 -0700

    Added _find_largest_rect method

--- bin/world.py
+++ bin/world.py
@@ -1,15 +1,15 @@
-from bin.random import Random;
-import numpy as np;
+from bin.random import Random
+import numpy as np
 
 # Emulate world generation
 class World:
     def __init__(self, seed=0, radius=20, min_size=8, spacing=1):
-        self._seed = seed;
-        self._spacing = spacing;
-        self._min_size = min_size;
-        self._random = Random();
-        self._slime_chunks = [];
-        self._search(radius);
+        self._seed = seed
+        self._spacing = spacing
+        self._min_size = min_size
+        self._random = Random()
+        self._slime_chunks = []
+        self._search(radius)
 
     # Determine if there is a slime chunk at x, z
     def _is_slime_chunk(self, xPosition, zPosition):
@@ -20,84 +20,124 @@ class World:
           np.int32(xPosition * 0x5ac0db) +
           np.int32(zPosition * zPosition) * 0x4307a7 +
           np.int32(zPosition * 0x5f24f) ^ 0x3ad8025f
-        );
+        )
 
         # Determine if slime chunk
-        return self._random.next_int(10) == 0;
+        return self._random.next_int(10) == 0
 
     # Search radius around 0, 0 for slime chunk clusters
     def _search(self, radius):
-        half_radius = int(radius / 2);
+        half_radius = int(radius / 2)
         for z in range(-half_radius, half_radius, self._spacing):
             for x in range(-half_radius, half_radius, self._spacing):
-                self._get_cluster(x, z, True);
+                self._get_cluster(x, z, True)
 
     
     # Organize chunks into histogram
     def _create_submatrix_histogram(self, chunks):
 
         # Initialize array and set all values to 0
-        histogram = [0 for i in range(len(chunks[0]))];
-        max_dimensions = (0, 0);
+        histogram = [0 for i in range(len(chunks[0]))]
+        max_dimensions = (0, 0)
 
         # Increment each element if multiple y chunks ( like histogram values )
         for x in range(0, len(chunks)):
             for z in range(0, len(chunks[0])):
                 histogram[z]+1 if chunks[x][z] else 0
 
-            temp = 0;  # Find largest rectangle in region cluster
-            if (temp[0] * temp[1] > max_dimensions[0] * max_dimensions[1]) max_dimensions = temp;
+            temp = self._find_largest_rect(histogram)  # Find largest rectangle in region cluster
+            if (temp[0] * temp[1] > max_dimensions[0] * max_dimensions[1]):
+                max_dimensions = temp
 
         # Return dimensions of largest rect
-        return max_dimensions;
+        return max_dimensions
+
+
+    # Find largest rect in submatrix histogram and return dimensions
+    def _find_largest_rect(self, histogram):
+        temp_pos = 0
+        max_size = float('-inf')
+        p_stack = []
+        h_stack = []
+        dimensions = (0, 0)
+
+        for i in range(0, len(histogram)):
+            h = histogram[i]
+
+            if not p_stack or h > h_stack[-1]:
+                h_stack.append(h)
+                p_stack.append(i)
+            elif h < h_stack[-1]:
+                while not h_stack and h < h_stack[-1]:
+                    temp_h = h_stack.pop()
+                    temp_pos = p_stack.pop()
+                    temp_size = temp_h * (i - temp_pos)
+                    max_size = max(temp_size, max_size)
+
+                    if max_size == temp_size:
+                        dimensions = (i - temp_pos, temp_h)
+
+                h_stack.append(h)
+                p_stack.append(temp_pos)
+
+        while not h_stack:
+            temp_h = h_stack.pop()
+            temp_pos = p_stack.pop()
+            temp_size = temp_h * (i - temp_pos)
+            max_size = max(temp_size, max_size)
+
+            if max_size == temp_size:
+                dimensions = (i - temp_pos, temp_h)
+
+        return dimensions
 
 
     # Recursively search for nearby slime chunks within cluster and return dimensions
     def _get_cluster(self, x, z, first=False):
 
         # If not slime chunk, exit
-        if not self._is_slime_chunk(x, z): return;
-        current_coords = (x, z);
+        if not self._is_slime_chunk(x, z): return
+        current_coords = (x, z)
 
         # If not slime chunk andand does not include current coordinates ( chunk hasn't been checked )
         if self._is_slime_chunk(x, z) and not current_coords in self._slime_chunks:
 
             # If first instance of class, clear coordinates
             if first:
-                self._slime_chunks.clear();
+                self._slime_chunks.clear()
 
             # Push self to checked chunks
-            self._slime_chunks.append(current_coords);
+            self._slime_chunks.append(current_coords)
             
             # Check sides 
-            self._get_cluster(x+1, z);
-            self._get_cluster(x-1, z);
-            self._get_cluster(x, z+1);
-            self._get_cluster(x, z-1);
+            self._get_cluster(x+1, z)
+            self._get_cluster(x-1, z)
+            self._get_cluster(x, z+1)
+            self._get_cluster(x, z-1)
 
             # Add cluster to set if size >= min_size
             if len(self._slime_chunks) >= self._min_size and first:
-                cluster_region = self._generate_cluster_region(self._slime_chunks);
-                largest_rect_dimensions = self._create_submatrix_histogram(cluster_region);
-                self._print_cluster_region(cluster_region);
+                cluster_region = self._generate_cluster_region(self._slime_chunks)
+                largest_rect_dimensions = self._create_submatrix_histogram(cluster_region)
+                self._print_cluster_region(cluster_region)
 
     # Generate 2D array representation of cluster
     def _generate_cluster_region(self, chunks):
 
         # Initialize bounding box
-        left = right = chunks[0][0];
-        top = bottom = chunks[0][1];
+        left = right = chunks[0][0]
+        top = bottom = chunks[0][1]
 
         # Find bounding box
         for c in chunks:
-            left = min(c[0], left);
-            right = max(c[0], right);
-            top = max(c[1], top);
-            bottom = min(c[1], bottom);
+            left = min(c[0], left)
+            right = max(c[0], right)
+            top = max(c[1], top)
+            bottom = min(c[1], bottom)
 
         # Bounding box dimensions
-        width = right + 1 - left;
-        height = top + 1 - bottom;
+        width = right + 1 - left
+        height = top + 1 - bottom
 
         # Initialize 2D array
         cluster_region = [[0 for i in range(width)] for j in range(height)]
@@ -106,23 +146,23 @@ class World:
         for z in range(height):
             for x in range(width):
                 c = (x+left, z+bottom)
-                cluster_region[z][x] = c in chunks;
+                cluster_region[z][x] = c in chunks
 
-        return cluster_region;
+        return cluster_region
 
 
     # Print cluster region
     def _print_cluster_region(self, cluster_region):
         for z in cluster_region:
             for x in z:
-                print('■ ' if x else '□ ', end='');
-            print("");
+                print('■ ' if x else '□ ', end='')
+            print("")
 
     # Print all slime chunks in radius
     def _print_map(self, radius):
-        half_radius = int(radius / 2);
+        half_radius = int(radius / 2)
         for z in range(-half_radius, half_radius):
             for x in range(-half_radius, half_radius):
-                print('■ ' if self._is_slime_chunk(x, z) else '□ ', end='');
-            print('');
+                print('■ ' if self._is_slime_chunk(x, z) else '□ ', end='')
+            print('')
         

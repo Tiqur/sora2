d06a9a9a6e6dc940902f8873f3e38943630f083b
Tiqur <Tiqur7@gmail.com>
2021-09-04 21:10:23 -0700

    Move search function to seperate function

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from bin.world import search;
 import datetime, random;
 
-start = datetime.datetime.now();
+# Search random seed
+def search_seed(seed):
+    start = datetime.datetime.now();
+    print("Searching: " + str(seed));
+    clusters = search(seed=seed, radius=10000, min_size=10, spacing=3);
+    print(datetime.datetime.now() - start);
+    # If cluster is found, POST to server
+
+
 
 # Generate random seed
 seed = random.randint(-9223372036854775808, 9223372036854775807);
-print("Searching: " + str(seed));
-clusters = search(seed=seed, radius=10000, min_size=10, spacing=3);
-
-print(datetime.datetime.now() - start);
+search_seed(seed);
